f43eedb1b3f232fc2a62889c6e6274af938b9c91
hellofairuzz <153610998+hellofairuzz@users.noreply.github.com>
2023-12-18 23:24:47 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,10 +5,18 @@ import streamlit as st
 # Header
 st.header('Fairuz :sparkles:')
 st.subheader('Plot')
- 
+
+nama = st.text_input('Nama', 'Nugroho', label_visibility='collapse')
+st.write('Halo ', nama)
+
+f1 = st.number_input('f1 = ', value=1)
+f2 = st.number_input('f2 = ', value=1)
+st.write('Frekuensi plot sinus adalah ', f1)
+st.write('Frekuensi plot cosinus adalah ', f2)
+
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(x)  # Calculating sin(x) values
-z = np.cos(x)  # Calculating sin(x) values
+y = np.sin(f1 * x)  # Calculating sin(x) values
+z = np.cos(f2 * x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
